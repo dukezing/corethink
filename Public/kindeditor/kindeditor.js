6c5cad81f0dcc6d9ce63208dabfa99f5ac8f13c8
JiangRuYi <598821125@qq.com>
2015-06-01 22:11:39 +0800

    修复：FormBuilder日期为空时显示1900 修复：FormBuilder图片上传名称错误 改进：修改Kindeditor默认工具栏按钮列表（加上下载远程图片和投票插件）

--- Public/kindeditor/kindeditor.js
+++ Public/kindeditor/kindeditor.js
@@ -256,13 +256,13 @@ K.options = {
     zIndex : 811213,
     items : [
         'source', '|', 'undo', 'redo', '|', 'preview', 'print', 'template', 'code', 'cut', 'copy', 'paste',
-        'plainpaste', 'wordpaste', '|', 'justifyleft', 'justifycenter', 'justifyright',
+        'plainpaste', 'wordpaste', 'downremoteimg', '|', 'justifyleft', 'justifycenter', 'justifyright',
         'justifyfull', 'insertorderedlist', 'insertunorderedlist', 'indent', 'outdent', 'subscript',
         'superscript', 'clearhtml', 'quickformat', 'selectall', '|', 'fullscreen', '/',
         'formatblock', 'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold',
         'italic', 'underline', 'strikethrough', 'lineheight', 'removeformat', '|', 'image', 'multiimage',
         'flash', 'media', 'insertfile', 'table', 'hr', 'emoticons', 'baidumap', 'pagebreak',
-        'anchor', 'link', 'unlink', '|', 'about'
+        'anchor', 'link', 'unlink', 'poll', '|', 'about'
     ],
     noDisableItems : ['source', 'fullscreen'],
     colorTable : [
