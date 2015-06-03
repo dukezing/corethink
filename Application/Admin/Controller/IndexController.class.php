21b5e4758ba99d5695851ab5153194bd801c936d
JiangRuYi <598821125@qq.com>
2015-06-04 02:46:51 +0800

    优化：Daterangepicker替换Datetimepicker 新增：后台首页统计图支持时间段选择

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -19,11 +19,13 @@ class IndexController extends AdminController{
      */
     public function index(){
         $today = strtotime(date('Y-m-d', time())); //今天
-        $count_day = 15; //查询最近n天
+        $start_date = I('get.start_date') ? I('get.start_date')/1000 : $today-10*86400;
+        $end_date   = I('get.end_date') ? (I('get.end_date')+1)/1000 : $today+86400;
+        $count_day  = ($end_date-$start_date)/86400; //查询最近n天
         $user_object = D('User');
-        for($i = $count_day; $i--; $i >= 0){
-            $day = $today - $i * 86400; //n天前日期
-            $day_after = $today - ($i - 1) * 86400; //n-1天前日期
+        for($i = 0; $i < $count_day; $i++){
+            $day = $start_date + $i*86400; //第n天日期
+            $day_after = $start_date + ($i+1)*86400; //第n+1天日期
             $map['ctime'] = array(
                 array('egt', $day),
                 array('lt', $day_after)
@@ -31,6 +33,9 @@ class IndexController extends AdminController{
             $user_reg_date[] = date('m月d日', $day);
             $user_reg_count[] = (int)$user_object->where($map)->count();
         }
+
+        $this->assign('start_date', date('Y年m月d日', $start_date));
+        $this->assign('end_date', date('Y年m月d日', $end_date-1));
         $this->assign('count_day', $count_day);
         $this->assign('user_reg_date', json_encode($user_reg_date));
         $this->assign('user_reg_count', json_encode($user_reg_count));
