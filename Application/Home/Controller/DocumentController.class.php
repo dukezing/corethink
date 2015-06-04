0956456827d5e0892c9d3e845c97d80f0258d80d
JiangRuYi <598821125@qq.com>
2015-06-04 18:24:03 +0800

    修复：评论调用错误

--- Application/Home/Controller/DocumentController.class.php
+++ Application/Home/Controller/DocumentController.class.php
@@ -161,7 +161,7 @@ class DocumentController extends HomeController{
         if(!$result){
             $this->error($document_object->getError());
         }else{
-            $this->success($result['id']?'更新成功':'新增成功', U('Index/index'));
+            $this->success($result['id']?'更新成功':'新增成功', C('HOME_PAGE'));
         }
     }
 
