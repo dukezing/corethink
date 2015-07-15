e677886edf48573cde14234ec3e56a7deba7160d
JiangRuYi <598821125@qq.com>
2015-07-15 12:07:32 +0800

    修复：新建文档类型字段分组单引号导致换行失效

--- Application/Common/Model/DocumentTypeModel.class.php
+++ Application/Common/Model/DocumentTypeModel.class.php
@@ -33,7 +33,7 @@ class DocumentTypeModel extends Model{
     protected $_auto = array(
         array('field_sort', '{"1":["1","3"],"2":["10","8"]}', self::MODEL_INSERT),
         array('field_sort', 'json_encode', self::MODEL_UPDATE, 'function'),
-        array('field_group', '1:基础\n2:扩展', self::MODEL_INSERT),
+        array('field_group', "1:基础\n2:扩展", self::MODEL_INSERT),
         array('ctime', NOW_TIME, self::MODEL_INSERT),
         array('utime', NOW_TIME, self::MODEL_BOTH),
         array('status', '1', self::MODEL_INSERT),
